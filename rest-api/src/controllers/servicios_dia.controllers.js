a243ab52cece3ea0ffa0cff8ffa153ec935a7b53
Jyaru Hernandez <jyaru1110@gmail.com>
2023-03-09 10:53:06 -0600

    Ruta para obtener el servicio más proximo

--- rest-api/src/controllers/servicios_dia.controllers.js
+++ rest-api/src/controllers/servicios_dia.controllers.js
@@ -1,4 +1,6 @@
 const Servicios_dia = require('../models/Servicios_dia');
+const { Op } = require("sequelize");
+const sequelize = require('../database/database');
 
 const get_servicios_fecha = async (req, res) => {
     var fecha = req.query.fecha;
@@ -13,7 +15,21 @@ const get_servicios_fecha = async (req, res) => {
 }
 
 const get_proximo_servicio = async (req, res) => {
-    res.send("get_proximo_servicio");
+    const servicios = await Servicios_dia.findAll(
+        {
+            limit: 1,
+            order: [
+                ['fecha', 'ASC'],
+                ['hora_inicio', 'ASC']
+            ],
+            where:{
+                fecha:{
+                    [Op.gt]: Date.now()
+                }
+            }
+        }
+    )
+    res.send(servicios);
 }
 
 module.exports = {
