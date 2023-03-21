ce45f353c3a715b96bd76e4a1fbd396c3cfa9835
Jyaru Hernandez <jyaru1110@gmail.com>
2023-03-20 22:07:13 -0600

    Servicio modificado ui

--- react-app/src/pages/HomeGestor/components/Servicio.jsx
+++ react-app/src/pages/HomeGestor/components/Servicio.jsx
@@ -1,15 +1,15 @@
 export default function Servicio(props){
     const servicio = props.servicio_parm;
     return(
-        <div className="group/item w-full bg-secondary hover:bg-cyan-800 p-2 my-2 text-white rounded-xl flex justify-between items-center h-20">
+        <div className="group/item w-full bg-primary text-white p-2 my-2  rounded-xl flex justify-between items-center h-14">
             <div className="flex flex-row">
-                <p className="font-poppins font-thin text-sm">{servicio.hora_inicio.substring(0,5)} - {servicio.hora_fin.substring(0,5)}</p>
+                <p className="font-poppins font-thin text-xs">{servicio.hora_inicio.substring(0,5)} - {servicio.hora_fin.substring(0,5)}</p>
                 <div className="ml-3.5">
-                    <p className="font-poppins font-semibold text-sm">{servicio.salon_id}</p>
-                    <p className="font-poppins font-semibold text-sm">{servicio.estado}</p>
+                    <p className="font-poppins font-semibold text-xs">{servicio.salon_id}</p>
+                    <p className="font-poppins font-semibold text-xs">{servicio.estado}</p>
                     {servicio.num_servicios>0 ?
-                        <p className="font-poppins text-sm">{servicio.num_servicios} servicios</p>:
-                        <p className="font-poppins text-sm">Sin servicios</p>
+                        <p className="font-poppins text-xs">{servicio.num_servicios} servicios</p>:
+                        <p className="font-poppins text-xs">Sin servicios</p>
                     }
                 </div>
             </div>
