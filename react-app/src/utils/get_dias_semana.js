892aae654f0d845861f11d5e4e74de689b122a09
Jyaru Hernandez <jyaru1110@gmail.com>
2023-03-20 22:12:11 -0600

    Dias de la semana siempre son 6

--- react-app/src/utils/get_dias_semana.js
+++ react-app/src/utils/get_dias_semana.js
@@ -4,10 +4,12 @@ export const getDiasSemana = () => {
     var diaString = "";
     for (let i = 0; i < 6; i++) {
         const dia = new Date(hoy + i * 24 * 60 * 60 * 1000);
-        if(dia.getDay() !== 0)
+        if(dia.getDay() != 0)
         {
             diaString =  (dia.getMonth() + 1) + "/" +  dia.getDate() + "/" + dia.getFullYear();
             diasSemana.push(diaString);
+        }else{
+            i--;
         }
     }
     return diasSemana;
