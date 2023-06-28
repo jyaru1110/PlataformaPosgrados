d314842a22bc15478dc45ec4742756a502b1f229
Jyaru Hernandez <jyaru1110@gmail.com>
2023-06-28 01:16:35 -0600

    solicitudes para gestor solo devuelve los pendientes

--- rest-api/src/controllers/solicitudes.controllers.js
+++ rest-api/src/controllers/solicitudes.controllers.js
@@ -6,7 +6,7 @@ const get_solicitudes = async (req, res) => {
     const rol =  req.user.dataValues.rol;
     var query = "";
     if(rol == "Gestor"){
-        query = "select usuarios.nombre, notificaciones.programa, notificaciones.no_clase, notificaciones.salon, notificaciones.id, notificaciones.fecha_inicio, notificaciones.fecha_fin, notificaciones.hora_inicio, notificaciones.hora_fin, notificaciones.hora_servicio_inicio,notificaciones.hora_servicio_fin, notificaciones.dia, notificaciones.num_alumnos, notificaciones.estado from notificaciones inner join usuarios on usuarios.id = notificaciones.id_usuario order by notificaciones.fecha_inicio;";
+        query = "select usuarios.nombre, notificaciones.programa, notificaciones.no_clase, notificaciones.salon, notificaciones.id, notificaciones.fecha_inicio, notificaciones.fecha_fin, notificaciones.hora_inicio, notificaciones.hora_fin, notificaciones.hora_servicio_inicio,notificaciones.hora_servicio_fin, notificaciones.dia, notificaciones.num_alumnos, notificaciones.estado from notificaciones inner join usuarios on usuarios.id = notificaciones.id_usuario where notificaciones.estado ='Pendiente' order by notificaciones.fecha_inicio;";
     }else{
         query = "select usuarios.nombre, notificaciones.programa, notificaciones.no_clase, notificaciones.salon, notificaciones.id, notificaciones.fecha_inicio, notificaciones.fecha_fin, notificaciones.hora_inicio, notificaciones.hora_fin, notificaciones.hora_servicio_inicio,notificaciones.hora_servicio_fin, notificaciones.dia, notificaciones.num_alumnos, notificaciones.estado from notificaciones inner join usuarios on usuarios.id = notificaciones.id_usuario where notificaciones.id_usuario = "+req.user.dataValues.id+" order by notificaciones.fecha_inicio;";
     }
