bdf49e6bab60aaf6a914cc91fe7b52a0972f63f2
Jyaru Hernandez <jyaru1110@gmail.com>
2023-07-04 16:53:56 -0600

    trust proxy

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -34,6 +34,8 @@ app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
+app.set("trust proxy", 1);
+
 
 app.use(
   cookieSession({
