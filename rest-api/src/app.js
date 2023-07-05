8e54432bae2b8a9d78d215e612796100f7c10d92
Jyaru Hernandez <jyaru1110@gmail.com>
2023-07-05 10:03:56 -0600

    sin dominio

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -42,8 +42,7 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
     keys: ["session","session.sig"],
     secure: true,
-    sameSite: "none",
-    domain: "https://coffee-breaks.vercel.app",
+    sameSite: "none"
   })
 );
 
