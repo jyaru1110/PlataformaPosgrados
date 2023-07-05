21f277307d6acde37af14a32cf641e239589e3fd
Jyaru Hernandez <jyaru1110@gmail.com>
2023-07-04 21:44:01 -0600

    nuevos headers

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -17,7 +17,8 @@ const cors = require("cors");
 
 //permite que cualquier dominio pueda hacer peticiones a la api
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
