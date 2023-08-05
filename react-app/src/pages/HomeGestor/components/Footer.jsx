53eafa725a30466058a7f5a3b69aea94bb5576d8
Jyaru Hernandez <jyaru1110@gmail.com>
2023-08-04 19:56:55 -0600

    correccion de posicion de boton add

--- react-app/src/pages/HomeGestor/components/Footer.jsx
+++ react-app/src/pages/HomeGestor/components/Footer.jsx
@@ -6,7 +6,7 @@ import IconBuscarServicios from "../../../assets/buscar_servicios.svg";
 import IconSolicitudes from "../../../assets/solicitud_servicios.svg";
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 flex flex-col items-center w-full md:w-1/3 md:pr-16 md:top-1/4 lg:pr-20">
+    <div className="fixed bottom-0 left-0 flex flex-col items-center w-full md:w-1/3 md:pr-16 md:top-1/4 lg:pr-20 z-50">
       <ButtonAdd ruta="/add-servicio" />
       <div className="w-full p-4 overflow-x-auto flex md:flex-col md:items-center sm:justify-between">
         <BotonFooter
