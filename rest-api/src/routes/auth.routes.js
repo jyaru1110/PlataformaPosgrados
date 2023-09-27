aa39085c6ebd526ca616e1779b1391b434d4038b
Jyaru Hernandez <jyaru1110@gmail.com>
2023-09-27 11:36:15 -0600

    Nuevo scope para google drive

--- rest-api/src/routes/auth.routes.js
+++ rest-api/src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const callbackURL_front = process.env.CALLBACK_URL_FRONT;
 
-router.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email','https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive.resource'] }));
 
 router.get('/auth/google/callback', passport.authenticate('google', {
     failureMessage: 'No se pudo iniciar sesión con Google',
