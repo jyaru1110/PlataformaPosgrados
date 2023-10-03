bd10e94cecd2cd913dc42051ace12f8397efe80c
Jyaru Hernandez <jyaru1110@gmail.com>
2023-10-03 11:41:53 -0600

    campos disabled para solo lectura en forms

--- react-app/src/components/form/Fechas.jsx
+++ react-app/src/components/form/Fechas.jsx
@@ -14,11 +14,11 @@ export default function Fechas(props) {
         <div className="flex w-full justify-between">
             <div className="flex flex-col">
                 <label className="font-poppins text-sm mb-2 ml-1 font-thin">Fecha de inicio</label>
-                <input className="w-36 font-poppins text-sm h-7 bg-primarylight text-gray1 rounded-md px-2.5 mb-4" type="date" onChange={handleInicio} defaultValue={inicio}/>
+                <input className="w-36 font-poppins text-sm h-7 bg-primarylight text-gray1 rounded-md px-2.5 mb-4" type="date" onChange={handleInicio} defaultValue={inicio} disabled={props.disabled}/>
             </div>
             <div className="flex flex-col">
                 <label className="font-poppins text-sm mb-2 ml-1 font-thin">Fecha de fin</label>
-                <input className="w-36 font-poppins text-sm h-7 bg-primarylight text-gray1 rounded-md px-2.5 mb-4" type="date" onChange={handleFin} defaultValue={fin}/>
+                <input className="w-36 font-poppins text-sm h-7 bg-primarylight text-gray1 rounded-md px-2.5 mb-4" type="date" onChange={handleFin} defaultValue={fin} disabled={props.disabled}/>
             </div>
         </div>
     );
