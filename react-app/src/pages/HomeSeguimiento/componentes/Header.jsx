b95a2b803223ac03f654bd972c30f7d4e7e1e13e
Jyaru Hernandez <jyaru1110@gmail.com>
2023-10-04 15:33:57 -0600

    correción de ruta de logo de posgrados y findAll en vez de findOne

--- react-app/src/pages/HomeSeguimiento/componentes/Header.jsx
+++ react-app/src/pages/HomeSeguimiento/componentes/Header.jsx
@@ -1,4 +1,4 @@
-import LogoPosgrados from "../../..assets/logoposgrados.png";
+import LogoPosgrados from "../../../assets/logoposgrados.png";
 
 export default function Header(props) {
   return (
--- rest-api/src/controllers/proceso.controller.js
+++ rest-api/src/controllers/proceso.controller.js
@@ -1,10 +1,11 @@
 const Proceso = require("../models/Proceso");
 const EtapaProceso = require("../models/EtapaProceso");
 const Etapa = require("../models/Etapa");
+const Programa = require("../models/Programa");
 
 const get_procesos = async (req, res) => {
-  const procesos = await Proceso.findOne({
-    include:[Etapa]
+  const procesos = await Proceso.findAll({
+    include:[Etapa,Programa]
   });
   res.status(200).send({ procesos: procesos });
 };
