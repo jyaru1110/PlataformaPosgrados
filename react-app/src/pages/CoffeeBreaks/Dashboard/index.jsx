dea0f316a96fc67b7330ea0ef5777fd9bb42d5ea
Jyaru Hernandez <jyaru1110@gmail.com>
2024-02-23 16:35:35 -0600

    add fechas funcionales

--- react-app/src/pages/CoffeeBreaks/Dashboard/index.jsx
+++ react-app/src/pages/CoffeeBreaks/Dashboard/index.jsx
@@ -1,11 +1,13 @@
 import Header from "../../../components/Header";
 import Fechas from "../../../components/form/Fechas";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useServiciosImpuntuales } from "../../../hooks/useServiciosImpuntuales";
 import { useProgramasServiciosImpuntuales } from "../../../hooks/useProgramasServiciosImpuntuales";
 import { useServiciosAprobados } from "../../../hooks/useServiciosAprobados";
 import { useServiciosCancelados } from "../../../hooks/useServiciosCancelados";
 import { useNivelImpuntualidad } from "../../../hooks/useNivelImpuntualidad";
+import { useSearchParams } from "react-router-dom";
+import { getSemanaPasada } from "../../../utils/get_dias_semana";
 import { Pie, Bar } from "react-chartjs-2";
 import MultiselectPrograma from "./Components/MultiselectProgram";
 import {
@@ -32,6 +34,30 @@ ChartJS.register(
 );
 
 export default function Dashboard() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const options_bar = {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Tiempo antes del servicio",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Número de servicios",
+        },
+      },
+    },
+  };
+
+  const { lunes, sabado } = getSemanaPasada();
+
+  const fecha_inicio = searchParams.get("fecha_inicio") || lunes;
+  const fecha_fin = searchParams.get("fecha_fin") || sabado;
+
   const [escuelas, setEscuelas] = useState([
     "Gobierno y Economía",
     "Bellas Artes",
@@ -48,39 +74,37 @@ export default function Dashboard() {
 
   const { data_nivel_impuntualidad, loading_nivel_impuntualidad } =
     useNivelImpuntualidad({
-      fecha_inicio: "2024-02-12",
-      fecha_fin: "2024-02-17",
+      fecha_inicio: fecha_inicio,
+      fecha_fin: fecha_fin,
       escuelas: escuelas,
     });
 
   const { servicios_impuntuales, loading_impuntuales } =
     useServiciosImpuntuales({
-      fecha_inicio: "2024-02-12",
-      fecha_fin: "2024-02-17",
+      fecha_inicio: fecha_inicio,
+      fecha_fin: fecha_fin,
       escuelas: escuelas,
     });
 
   const { servicios_aprobados, loading_aprobados } = useServiciosAprobados({
-    fecha_inicio: "2024-02-12",
-    fecha_fin: "2024-02-17",
+    fecha_inicio: fecha_inicio,
+    fecha_fin: fecha_fin,
     escuelas: escuelas,
   });
 
   const { programas_impuntuales, loading_programas_impuntuales } =
     useProgramasServiciosImpuntuales({
-      fecha_inicio: "2024-02-12",
-      fecha_fin: "2024-02-17",
-      escuelas:escuelas,
+      fecha_inicio: fecha_inicio,
+      fecha_fin: fecha_fin,
+      escuelas: escuelas,
     });
 
   const { servicios_cancelados, loading_servicios_cancelados } =
     useServiciosCancelados({
-      fecha_inicio: "2024-02-12",
-      fecha_fin: "2024-02-17",
+      fecha_inicio: fecha_inicio,
+      fecha_fin: fecha_fin,
       escuelas: escuelas,
     });
-
-    console.log(servicios_cancelados);
 
   const onChangePrograma = (array) => {
     setEscuelas(array.map((item) => item.label));
@@ -90,7 +114,18 @@ export default function Dashboard() {
     <div className="font-poppins p-7">
       <Header titulo="Dashboard"></Header>
       <MultiselectPrograma onChange={onChangePrograma}></MultiselectPrograma>
-      <Fechas></Fechas>
+      <Fechas
+        value_inicio={fecha_inicio}
+        value_fin={fecha_fin}
+        setFechaInicio={(fecha_inicio_value) => {
+          searchParams.set("fecha_inicio", fecha_inicio_value);
+          setSearchParams(searchParams);
+        }}
+        setFechaFin={(fecha_fin_value) => {
+          searchParams.set("fecha_fin", fecha_fin_value);
+          setSearchParams(searchParams);
+        }}
+      ></Fechas>
       <div className="w-full flex flex-wrap justify-between p-5 rounded-2xl bg-primarylight shadow-lg">
         <div className="w-80">
           <p className="text-center font-bold">SERVICIOS SOLICITADOS</p>
@@ -129,8 +164,10 @@ export default function Dashboard() {
         "Cargando..."
       ) : (
         <div className="w-full p-7">
-          <p className="text-center font-bold">NIVEL DE IMPUNTUALIDAD EN SOLICITUDES</p>
-          <Bar data={data_nivel_impuntualidad} />
+          <p className="text-center font-bold">
+            SOLICITUDES
+          </p>
+          <Bar data={data_nivel_impuntualidad} options={options_bar} />
         </div>
       )}
     </div>
--- react-app/src/utils/get_dias_semana.js
+++ react-app/src/utils/get_dias_semana.js
@@ -1,20 +1,29 @@
 export const getDiasSemana = () => {
-    const hoy = Date.now();
-    const diasSemana = [];
-    var diaString = "";
-    var limite = 6;
-    for (var i = 0; i < limite; i++)
-    {
-        const dia = new Date(hoy + i * 24 * 60 * 60 * 1000);
-        if(dia.getDay() !== 0)
-        {
-            diaString = (dia.getMonth() + 1) + "/" +  dia.getDate() + "/" + dia.getFullYear();
-            diasSemana.push(diaString);
-        }
-        else
-        {
-            limite++;
-        }
+  const hoy = Date.now();
+  const diasSemana = [];
+  var diaString = "";
+  var limite = 6;
+  for (var i = 0; i < limite; i++) {
+    const dia = new Date(hoy + i * 24 * 60 * 60 * 1000);
+    if (dia.getDay() !== 0) {
+      diaString =
+        dia.getMonth() + 1 + "/" + dia.getDate() + "/" + dia.getFullYear();
+      diasSemana.push(diaString);
+    } else {
+      limite++;
     }
-    return diasSemana;
-}
+  }
+  return diasSemana;
+};
+
+export const getSemanaPasada = () => {
+  const hoy = new Date();
+  const numero_dia = hoy.getDay();
+  const lunes_pasado = new Date(
+    hoy - (7 + numero_dia - 1) * 24 * 60 * 60 * 1000
+  );
+  const sabado_pasado = new Date(
+    hoy - (7 + numero_dia - 6) * 24 * 60 * 60 * 1000
+  );
+  return {lunes:lunes_pasado.toISOString().split("T")[0], sabado:sabado_pasado.toISOString().split("T")[0]};
+};
