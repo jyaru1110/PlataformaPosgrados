eb2c1ccbf3733f69e11914d27baa99034599df82
Jyaru Hernandez <jyaru1110@gmail.com>
2024-03-17 15:52:58 -0600

    add endpoint para obtener a todos los usuarios

--- rest-api/src/routes/user.routes.js
+++ rest-api/src/routes/user.routes.js
@@ -3,7 +3,7 @@ const { isUserAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
 const Usuario = require("../models/Usuario");
 
-router.get("/user/auth",isUserAuthenticated,(req, res) => {
+router.get("/user/auth", isUserAuthenticated, (req, res) => {
   if (req.user) {
     res.send(req.user);
   } else {
@@ -11,49 +11,63 @@ router.get("/user/auth",isUserAuthenticated,(req, res) => {
   }
 });
 
-router.put('/user/gestor/:id',async (req, res) => {
-    const id = req.params.id;
-    try {
-        const user = await Usuario.update(
-
-            {
-                rol:'Gestor'
-            },
-            {
-                where:{
-
-                    id:id
-                }
-            }
-        )
-        res.status(200).send({user:user});
-    } catch (error) {
-        res.status(500).send({error:error});
-    }
-})
-
-router.put('/user/usuario/:id',async (req, res) => {
+router.put("/user/gestor/:id", async (req, res) => {
   const id = req.params.id;
   try {
-      const user = await Usuario.update(
-          {
-              rol:'Usuario'
-          },
-          {
-
-              where:{
-
-                  id:id
-              }
-          }
-      )
-      res.status(200).send({user:user});
+    const user = await Usuario.update(
+      {
+        rol: "Gestor",
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.status(200).send({ user: user });
   } catch (error) {
-      res.status(500).send({error:error});
+    res.status(500).send({ error: error });
   }
-})
+});
 
+router.put("/user/usuario/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await Usuario.update(
+      {
+        rol: "Usuario",
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.status(200).send({ user: user });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
 
-
+router.get("/user/all", async (req, res) => {
+  try {
+    const users = await Usuario.findAll({
+        attributes: [
+            'id',
+            'titulo',
+            'nombre',
+            'escuela',
+            'email',
+            'extension',
+            'birthday',
+            'telefono',
+            'foto'
+        ]
+    });
+    res.status(200).send({ users: users });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
 
 module.exports = router;
