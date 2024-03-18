43ed2fffeed9d1a7cd5f0ec81ef8c021c7268ccd
Jyaru Hernandez <jyaru1110@gmail.com>
2024-03-18 16:58:13 -0600

    add endpoint get user

--- rest-api/src/routes/user.routes.js
+++ rest-api/src/routes/user.routes.js
@@ -52,20 +52,34 @@ router.put("/user/usuario/:id", async (req, res) => {
 router.get("/user/all", async (req, res) => {
   try {
     const users = await Usuario.findAll({
-        attributes: [
-            'id',
-            'titulo',
-            'nombre',
-            'escuela',
-            'email',
-            'extension',
-            'birthday',
-            'telefono',
-            'foto',
-            'id_universidad_panamericana'
-        ]
+      attributes: [
+        "id",
+        "titulo",
+        "nombre",
+        "escuela",
+        "email",
+        "extension",
+        "birthday",
+        "telefono",
+        "foto",
+        "id_universidad_panamericana",
+      ],
     });
     res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await Usuario.findOne({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).send(user);
   } catch (error) {
     res.status(500).send(error);
   }
