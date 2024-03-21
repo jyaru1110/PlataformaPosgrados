d960ab2ea116f1e80e26c727fb0f6e2ee697f9b1
Jyaru Hernandez <jyaru1110@gmail.com>
2024-03-21 13:08:33 -0600

    nuevos campos en aperturas

--- rest-api/src/models/AperturasCierres.js
+++ rest-api/src/models/AperturasCierres.js
@@ -1,24 +1,34 @@
-const { DataTypes } = require('sequelize');
-var sequelize = require('../database/database');
+const { DataTypes } = require("sequelize");
+var sequelize = require("../database/database");
 
-const AperturasCierres = sequelize.define('aperturas_cierres', {
+const AperturasCierres = sequelize.define(
+  "aperturas_cierres",
+  {
     // Atributos del modelo
     fecha_inicio: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     fecha_fin: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     term: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-}, {
+    meta_inscripciones: {
+      type: DataTypes.INTEGER,
+    },
+    num_inscripciones: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
     // Opciones del modelo
-    tableName: 'aperturas_cierres',
-    timestamps: false
-});
+    tableName: "aperturas_cierres",
+    timestamps: false,
+  }
+);
 
 module.exports = AperturasCierres;
