54b2ec66763e32a2c8abce4024fb44655f4278d6
Jyaru Hernandez <jyaru1110@gmail.com>
2024-04-19 19:19:02 -0600

    add funcion reverse_date para funcionamiento con la bd nueva

--- react-app/src/hooks/useServiciosDia.js
+++ react-app/src/hooks/useServiciosDia.js
@@ -1,26 +1,32 @@
-import { useState, useEffect } from 'react';
-import {get_fetch}  from './get_fetch';
+import { useState, useEffect } from "react";
+import { get_fetch } from "./get_fetch";
 
-const url_backend  = import.meta.env.VITE_URL_API;
+const url_backend = import.meta.env.VITE_URL_API;
 
 export const useServiciosDia = (dia) => {
-    const [servicios, setServicios] = useState();
-    const [loading, setLoading] = useState(true);
+  const [servicios, setServicios] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const after_fetch = (data) => {
-        setServicios(data.servicio[0]);
-        setLoading(false);
-    }
+  const after_fetch = (data) => {
+    setServicios(data.servicio[0]);
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        setLoading(true);
-        dia = dia.replace(/\//g, "-");
-        let url = `${url_backend}/servicios_pendientes/${dia}`;
-        const abortController = new AbortController();
-        const signal = abortController.signal;
-        get_fetch(url,signal, after_fetch);
-        return () => abortController.abort();
-    }, [dia]);
+  const reverse_date = (date) => {
+    const array = date?.split("/");
 
-    return {servicios:servicios,loading:loading};
-}
+    const res = array[2] + "-" + array[0] + "-" + array[1];
+    return res;
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    let url = `${url_backend}/servicios_pendientes/${reverse_date(dia)}`;
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    get_fetch(url, signal, after_fetch);
+    return () => abortController.abort();
+  }, [dia]);
+
+  return { servicios: servicios, loading: loading };
+};
