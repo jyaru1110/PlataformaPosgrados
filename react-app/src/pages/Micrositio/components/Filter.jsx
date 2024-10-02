3da865fe01282b8a0caedc693450f8f77500e0a6
Jyaru Hernandez <jyaru1110@gmail.com>
2024-10-01 20:34:48 -0600

    refactor: no index en map

--- react-app/src/pages/Micrositio/components/Filter.jsx
+++ react-app/src/pages/Micrositio/components/Filter.jsx
@@ -22,7 +22,7 @@ export default function Filter({ title, options, filtered, setFiltered }) {
       </button>
       {isSelected && (
         <div className="flex flex-col absolute top-10 bg-white p-2 rounded-lg border-grayborder border w-56 z-50">
-          {options.map((option, index) => (
+          {options.map((option) => (
             <span key={option} className="flex items-center space-x-3">
               <input
                 id={option}
