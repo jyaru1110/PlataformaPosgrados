d38b92be0c4b86dc41c278714cc1719c91642d25
Jyaru Hernandez <jyaru1110@gmail.com>
2024-10-03 14:04:48 -0600

    style: mejora en el estilo del filtro

--- react-app/src/pages/Micrositio/components/Filter.jsx
+++ react-app/src/pages/Micrositio/components/Filter.jsx
@@ -30,12 +30,13 @@ export default function Filter({ title, options, filtered, setFiltered }) {
         {title}
       </button>
       {isSelected && (
-        <div className="flex flex-col absolute top-10 bg-white p-2 rounded-lg border-grayborder border w-56 z-50">
+        <div className="flex flex-col absolute top-10 bg-white py-2 px-1 rounded-lg border-grayborder border w-max z-50">
           {options.map((option) => (
-            <span key={option} className="flex items-center space-x-3">
+            <label htmlFor={option} className="flex items-center space-x-3 hover:bg-grayborder px-1 py-0.5 rounded-md" key={option} >
               <input
                 id={option}
                 type="checkbox"
+                className="mr-2"
                 checked={filtered.includes(option)}
                 onChange={(e) => {
                   if (e.target.checked) {
@@ -47,8 +48,8 @@ export default function Filter({ title, options, filtered, setFiltered }) {
                   }
                 }}
               />
-              <label htmlFor={option}>{option}</label>
-            </span>
+              {option}
+            </label>
           ))}
         </div>
       )}
