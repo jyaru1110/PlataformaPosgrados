acb9c5849a4a728cdbd5af331be3a55ae1dd2dd7
Jyaru Hernandez <jyaru1110@gmail.com>
2024-11-22 12:43:44 -0600

    fix: botones de enlaces externos

--- react-app/src/pages/Micrositio/Programas/Components/Accordion.jsx
+++ react-app/src/pages/Micrositio/Programas/Components/Accordion.jsx
@@ -3,18 +3,18 @@ export default function Accordion({ header, children, style, arrowColor}) {
   const [open, setOpen] = useState(false);
   return (
     <div
-      className={`${style} py-6 px-8 h-auto rounded-lg transition-all flex flex-col`}
+      className={`${style} py-6 px-8 rounded-lg flex flex-col`}
     >
       <div
         className={`cursor-pointer flex items-center justify-between w-full`}
         onClick={() => setOpen(!open)}
       >
         {header}
-        <svg className={`rotate-180 transition-transform ${open && "rotate-0"}`} xmlns="http://www.w3.org/2000/svg" width="17.572" height="9.599" viewBox="0 0 17.572 9.599">
+        <svg className={`transition-all ${open?"rotate-0":"rotate-180"}`} xmlns="http://www.w3.org/2000/svg" width="17.572" height="9.599" viewBox="0 0 17.572 9.599">
           <path d="M3040.993,122.59l-7.485,7.671,7.485,8.544" transform="translate(139.464 -3032.146) rotate(90)" fill="none" stroke={arrowColor} strokeWidth="2"/>
         </svg>
       </div>
-      {open && <div className="mt-5 h-auto space-y-4">{children}</div>}
+      {open && <div className="mt-5 space-y-4">{children}</div>}
     </div>
   );
 }
