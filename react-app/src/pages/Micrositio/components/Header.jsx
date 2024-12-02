d35fc3ef850648808e03aaafd007f2846b4b25d7
Jyaru Hernandez <jyaru1110@gmail.com>
2024-12-01 20:45:48 -0600

    style: header estilado correcto

--- react-app/src/pages/Micrositio/components/Header.jsx
+++ react-app/src/pages/Micrositio/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     <header className="w-full py-5 px-8 border shadow-header flex items-center justify-end space-x-5 sticky z-50">
       <span className="flex flex-col text-right">
         <span className="font-timesnr text-2xl text-primary">{localStorage.getItem("nombre")}</span>
-        <span className="font-timesnr text-xl text-secondary">{localStorage.getItem("escuela")||localStorage.getItem("area")}</span>
+        <span className="font-timesnr text-xl text-secondary">{localStorage.getItem("area") || localStorage.getItem("escuela")}</span>
       </span>
     <img
       className="rounded-full"
