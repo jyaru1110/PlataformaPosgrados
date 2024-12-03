9f13edbfbbe2e66b36037b461aa9378b95dc2dcb
Jyaru Hernandez <jyaru1110@gmail.com>
2024-12-03 11:20:15 -0600

    style: opciones en filtro con mas espacio

--- react-app/src/pages/Micrositio/components/Filter.jsx
+++ react-app/src/pages/Micrositio/components/Filter.jsx
@@ -30,7 +30,7 @@ export default function Filter({ title, options, filtered, setFiltered }) {
         {title}
       </button>
       {isSelected && (
-        <div className="flex flex-col absolute top-10 bg-white py-2 px-1 rounded-lg border-secondary border w-max z-50">
+        <div className="flex flex-col absolute top-12 bg-white py-2 px-1 rounded-lg border-secondary border w-max z-50">
           {options.map((option) => (
             <label htmlFor={option} className="flex items-center space-x-3 hover:bg-secondary/10 px-1 py-0.5 text-secondary rounded-md" key={option} >
               <input
